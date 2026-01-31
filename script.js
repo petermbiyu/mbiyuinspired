780ec2c15a3f0c9cc42301d10c4556abb68e044c
peter <petersmasha@gmail.com>
2026-01-31 09:04:41 +0300

    added courosel and other pages

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  $(".slider-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    dots: true,
+
+    infinite: true,
+    pauseOnHover: true,
+    touchThreshold: 10,
+    swipeToSlide: true,
+    lazyLoad: "ondemand",
+    speed: 300,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 810,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
